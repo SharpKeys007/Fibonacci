a641de979beb96878092a11e2b5005d96a2272f4
RandomKula <115494340+RandomKula@users.noreply.github.com>
2023-06-24 17:52:18 +0530

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Fibonacci
+{
+    public static int GetFibonacci(int num)
+    {
+        if (num < 0)
+        {
+            throw new InvalidDataException("Value cannot be negative");
+        }
+
+        if (num == 0) { return 0; }
+
+        if (num == 1) { return 1; }
+
+        return GetFibonacci(num - 1) + GetFibonacci(num - 2);
+    }
+
+    public static int GetWrongFibonacci(int num)
+    {
+        return -1;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter number to calculate Fibonacci");
+
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            Console.WriteLine($"Fibonacci of {n} = " + GetFibonacci(n));
+        }
+        else
+        {
+            throw new InvalidDataException("Value should be an integer");
+        }
+
+    }
+}
---
+++ FibonacciTests/FibonacciTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class FibonacciTests
+    {
+        [TestMethod()]
+        public void GetFibonacciTest()
+        {
+            Assert.AreEqual(0, Fibonacci.GetFibonacci(0));
+            Assert.AreEqual(5, Fibonacci.GetFibonacci(5));
+            Assert.AreEqual(8, Fibonacci.GetFibonacci(6));
+            Assert.AreEqual(10946, Fibonacci.GetFibonacci(21));
+        }
+    }
+}
